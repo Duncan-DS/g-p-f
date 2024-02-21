52dc2298149932d39bf2c7f13e1f76e19c20b8ac
Duncan-DS <schwartzanatar17@gmail.com>
2024-02-21 10:44:53 +0200

    Added search feature usability with data returning

--- git-prof-finder/src/components/pages/Home/Home.js
+++ git-prof-finder/src/components/pages/Home/Home.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Home.css';
+import axios from 'axios';
 
 const Home = () => {
+
+    const [query, setQuery] = useState("");
+
+    const [users, setUsers] = useState([]);
+
+    const handleQueryInput = (e) => {
+        const value = e.target.value;
+        setQuery(value);
+    }
+
+    const fetchUsers = async () => {
+        try {
+            const response = await axios.get("/search/users?q=" + query);
+            console.log(response)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const handleSearchUsers = async (e) => {
+        e.preventDefault();
+        await fetchUsers();
+    };
+
     return (
         <div className='container'>
             <div className='search-from'>
                 <h2>Github Search User</h2>
                 <form>
-                    <input type='text' />
-                    <button>Search</button>
+                    <input value={query} onChange={handleQueryInput} type='text' />
+                    <button onClick={handleSearchUsers}>Search</button>
                 </form>
             </div>
             <div className='search-results'>
+
                 <div className='user'>
                     <div className='image'>
                         <img
