ce02171c849e0eba80fa35fd613786fc9a467ccc
Duncan-DS <schwartzanatar17@gmail.com>
2024-02-22 07:47:42 +0200

    Created UI component with user script

---
+++ git-prof-finder/src/components/ui/User.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const User = ({user}) => {
+
+    const { avatar_url, login, id } = user;
+
+    return (
+        <div className='user'>
+            <div className='image'>
+                <img
+                    src={avatar_url}
+                    alt={login}
+                />
+            </div>
+            <div className='user-info'>
+                <h3>{login}</h3>
+                <small>{id}</small>
+                <Link to={`/user/${login}`}>View Profile</Link>
+            </div>
+        </div>
+    )
+};
+
+export default User;
