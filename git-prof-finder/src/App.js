ad4db6e18381853caed43ce633063d3b126b4bd0
Duncan-DS <schwartzanatar17@gmail.com>
2024-02-22 09:13:44 +0200

    reworked user

--- git-prof-finder/src/App.js
+++ git-prof-finder/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/user' element={<User />} />
+        <Route path='/user/:login' element={<User />} />
       </Routes>
     </BrowserRouter>
   );
