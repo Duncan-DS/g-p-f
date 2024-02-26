c4cb1618259fc522f63de301e7cacd9c3497bc34
Duncan-DS <schwartzanatar17@gmail.com>
2024-02-26 10:05:38 +0200

    Reworked coding strructure

--- git-prof-finder/src/components/ui/Repo.js
+++ git-prof-finder/src/components/ui/Repo.js
@@ -1,20 +1,15 @@
 import React from 'react';
 
-const Repo = ({repo}) => {
-
-    const { name, html_url, description, language } = repo; 
+const Repo = ({ repo }) => {
+    const { name, html_url, description, language } = repo;
 
     return (
         <div className='repo'>
-            <h3>
-                <a href={html_url}>{name}</a>
-            </h3>
-            <p>
-                {description}
-            </p>
-            { language && <small>Written in {language}</small>}
+            <h3><a href={html_url}>{name}</a></h3>
+            <p>{description}</p>
+            {language && <small>Written in {language}</small>}
         </div>
-    )
+    );
 };
 
 export default Repo;
